1a45e65918b9a0a3935f997e1b38f739dd16b8f9
Godswill Egegwu <egegwu.godswill@gmail.com>
2020-11-12 10:53:47 -0400

    Just a little comments

--- fullstack/import_cities.py
+++ fullstack/import_cities.py
@@ -4,15 +4,15 @@
 import csv
 #from smartcity.models import MyCity
 
-with open('data/worldcities.csv', encoding='utf8') as f:
-    res = csv.reader(f)
+with open('data/worldcities.csv', encoding='utf8') as f:  #imports file containing city tables called worldcities.csv
+    res = csv.reader(f)                                   #specifies format and reads in the worldcities.csv file
     count = 0
-    res = list(res)
+    res = list(res)                                       #processes instruction
     c = 0
     while c < 10:
-        print(res[c])
+        print(res[c])                                     #prints the result 'name' of cities read from the city table
         c +=1
 
 
-    
+    #ends import and polution of city table
  
